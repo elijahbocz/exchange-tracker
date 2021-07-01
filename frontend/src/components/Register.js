249989adbc40049df2aceba118f249550366d4ee
Elijah <boczelijah@gmail.com>
2021-06-30 23:01:03 -0400

    add registration page on frontend, registration function on backend

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import styled from "styled-components";
 
@@ -8,12 +8,59 @@ const StyledRegistration = styled.div`
   margin: 1rem;
 `;
 
+const StyledForm = styled.form`
+  margin: 1rem;
+  label {
+    margin: 1rem;
+  }
+`;
+
 function Register() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  function handleRegistration(e) {
+    e.preventDefault();
+    const credentials = { username: username, password: password};
+    fetch('http://127.0.0.1:5000/api/register-user', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(credentials),
+    })
+      .then(res => res.json())
+      .then(res => console.log(res));
+  }
+
+  function updateValues(e) {
+    if (e.target.id === "username") {
+      setUsername(e.target.value);
+    } else if (e.target.id === "password") {
+      setPassword(e.target.value);
+    } else if (e.target.id === "confirm-password") {
+      setConfirmPassword(e.target.value);
+    }
+  }
+
   return (
     <div className="home">
-      <Header />
+      <Header/>
       <StyledRegistration>
-        <p>Register:</p>
+        <p>Register</p>
+        <StyledForm onSubmit={handleRegistration}>
+          <label>Username</label>
+          <input type="text" id="username" onChange={updateValues}></input>
+          <p></p>
+          <label>Password</label>
+          <input type="password" id="password" onChange={updateValues}></input>
+          <p></p>
+          <label>Confirm Password</label>
+          <input type="password" id="confirm-password" onChange={updateValues}></input>
+          <p></p>
+          <button>Register</button>
+        </StyledForm>
       </StyledRegistration>
     </div>
   );
