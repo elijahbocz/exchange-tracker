0c7d29446ab0823678faf2b69df564f7ba1a3123
Elijah <boczelijah@gmail.com>
2021-08-13 18:24:44 -0400

    add comments to backend files

--- backend/db/add_coins_images.py
+++ backend/db/add_coins_images.py
@@ -3,13 +3,15 @@ import requests
 from db.connection import create_connection
 
 
+# function for fetching and adding the images for each available coin
 def add_coin_images():
     connection = create_connection()
     url = "https://api.coingecko.com/api/v3/coins/markets/"
     params = {"vs_currency": "usd"}
     res = requests.get(url, params=params).json()
+    # receives huge list of data (every coin in CoinGecko database)
     for coin in res:
-        print(coin['image'])
+        # we only care about the image associated
         with connection.cursor() as cursor:
             sql = "INSERT INTO `coins_images` (`coinID`, `imageLink`) VALUES (%s, %s)"
             cursor.execute(sql, (coin['id'], coin['image']))
