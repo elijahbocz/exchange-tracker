37da0d1bf626b0dc99e051e38a4f379964217fca
Elijah <boczelijah@gmail.com>
2021-10-19 12:00:02 -0400

    add favicon, get distinct items in simple_price on backend

--- backend/external/simple.py
+++ backend/external/simple.py
@@ -2,8 +2,12 @@ import requests
 
 # function for fetching the price of coins, using the coin id and currency
 def get_simple_price(ids, vs_currencies):
+    distinct_items = []
+    for item in ids:
+        if item not in distinct_items:
+            distinct_items.append(item)
     params = {
-        "ids": ids,
+        "ids": distinct_items,
         "vs_currencies": vs_currencies
     }
     url = "https://api.coingecko.com/api/v3/simple/price"
